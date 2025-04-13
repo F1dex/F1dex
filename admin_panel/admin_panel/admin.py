9fa95e7b343e77a7c99dab3a86e23943b2d4c61e
Mxlodix <153226500+MaxxMXSZ@users.noreply.github.com>
2025-04-13 23:27:55 +0200

    Update admin.py

--- admin_panel/admin_panel/admin.py
+++ admin_panel/admin_panel/admin.py
@@ -7,10 +7,14 @@ from django.contrib import admin
 
 class BallsdexAdminSite(admin.AdminSite):
     def login(self, request, extra_context=None):
-        img_dir = os.path.join(settings.BASE_DIR, "static/admin_backgrounds")
-        images = [f for f in os.listdir(img_dir) if f.endswith((".jpg", ".png"))]
+        img_dir = os.path.abspath(os.path.join(settings.BASE_DIR, "..", "static", "admin_backgrounds"))
+        try:
+            images = [f for f in os.listdir(img_dir) if f.endswith((".jpg", ".png"))]
+        except FileNotFoundError:
+            images = []
+    
         background_url = f"/static/admin_backgrounds/{random.choice(images)}" if images else None
-
+    
         if extra_context is None:
             extra_context = {}
         extra_context["background_url"] = background_url
