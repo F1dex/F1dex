0142c7eba7ba62b9d9112a7cf51009c5e679830b
Mxlodix <153226500+MaxxMXSZ@users.noreply.github.com>
2025-04-14 00:45:08 +0200

    Update admin.py

--- admin_panel/admin_panel/admin.py
+++ admin_panel/admin_panel/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.admin.sites import AdminSite
 from django.utils.translation import gettext_lazy as _
 
 
@@ -9,15 +8,3 @@ class BallsdexAdminSite(admin.AdminSite):
     site_url = None  # type: ignore
     login_template = "admin/login.html"
     final_catch_all_view = False
-
-    def each_context(self, request):
-        context = super().each_context(request)
-        return context
-
-    class Media:
-        css = {
-            'all': ['admin/css/f1dex_main.css'],
-        }
-
-admin.site = BallsdexAdminSite()
-admin.autodiscover()
