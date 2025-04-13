0d7155cde57d1511eb7219335aee4d3b5a31c107
Mxlodix <153226500+MaxxMXSZ@users.noreply.github.com>
2025-04-13 23:49:05 +0200

    Update admin.py

--- admin_panel/admin_panel/admin.py
+++ admin_panel/admin_panel/admin.py
@@ -1,21 +1,8 @@
-import os
-import random
-
-from django.conf import settings
 from django.contrib import admin
 
 
 class BallsdexAdminSite(admin.AdminSite):
-    def login(self, request, extra_context=None):
-        img_dir = os.path.abspath(os.path.join(settings.BASE_DIR, "..", "static", "admin_backgrounds"))
-        try:
-            images = [f for f in os.listdir(img_dir) if f.endswith((".jpg", ".png"))]
-        except FileNotFoundError:
-            images = []
-    
-        background_url = f"/static/admin_backgrounds/{random.choice(images)}" if images else None
-    
-        if extra_context is None:
-            extra_context = {}
-        extra_context["background_url"] = background_url
-        return super().login(request, extra_context)
+    site_header = "F1Dex Configuration"
+    site_title = "F1Dex admin panel"
+    site_url = None  # type: ignore
+    final_catch_all_view = False
