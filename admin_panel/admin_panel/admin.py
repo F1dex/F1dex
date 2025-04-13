8c6a7185ebc26aaa7aceec511fb6357c32d809fc
Mxlodix <153226500+MaxxMXSZ@users.noreply.github.com>
2025-04-14 00:16:20 +0200

    Update admin.py

--- admin_panel/admin_panel/admin.py
+++ admin_panel/admin_panel/admin.py
@@ -5,4 +5,5 @@ class BallsdexAdminSite(admin.AdminSite):
     site_header = "F1Dex Configuration"
     site_title = "F1Dex admin panel"
     site_url = None  # type: ignore
+    login_template = "admin/login.html"
     final_catch_all_view = False
